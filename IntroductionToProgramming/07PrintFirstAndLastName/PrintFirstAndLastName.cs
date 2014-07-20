cb0623c2f622ff01cc991cabeab89a6371b2bb3e
LorakR <stillhet@abv.bg>
2014-07-20 05:32:01 -0700

    .init

---
+++ IntroductionToProgramming/07PrintFirstAndLastName/PrintFirstAndLastName.cs
@@ -0,0 +1,21 @@
+﻿/*Create console application that prints your first and last name, each at a separate line.*/
+
+using System;
+
+class PrintFirstAndLastName
+{
+    static void Main()
+    {
+        //common way
+        Console.WriteLine("Pesho");
+        Console.WriteLine("Peshev");
+
+        //empty line for better visualisation
+        Console.WriteLine();
+
+        //with a placeholder and variable from type string
+        string firstName = "Pesho";
+        string lastName = "Peshev";
+        Console.WriteLine("{0}\n{1}", firstName, lastName);
+    }
+}
