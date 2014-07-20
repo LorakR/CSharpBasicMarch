92ff105dc60d13d4385250861deaa29a318ce7c9
LorakR <stillhet@abv.bg>
2014-07-20 05:24:08 -0700

    .init

---
+++ IntroductionToProgramming/05PrintMyName/PrintMyName.cs
@@ -0,0 +1,20 @@
+﻿/* Modify the previous application to print your name. */
+
+using System;
+
+class PrintMyName
+{
+    static void Main()
+    {
+        //with placeholder 
+        string myName = "Anonimcho";
+        Console.WriteLine("{0}", myName);
+
+        //with variable 
+        Console.WriteLine(myName);
+
+        //common way
+        Console.WriteLine("Anonimcho");
+    }
+}
+
---
+++ IntroductionToProgramming/06PrintNumbers/PrintNumbers.cs
@@ -0,0 +1,20 @@
+﻿/*Write a program to print the numbers 1, 101 and 1001, each at a separate line.*/
+
+using System;
+
+class PrintNumbers
+{
+    static void Main()
+    {
+        //Common way
+        Console.WriteLine("1");
+        Console.WriteLine("101");
+        Console.WriteLine("1001");
+
+        //Empty line for better visualisation
+        Console.WriteLine("");
+
+        //With placeholder --> \n insert new line
+        Console.WriteLine("{0}\n{1}\n{2}", 1, 101, 1001);
+    }
+}
