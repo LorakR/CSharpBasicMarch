102758db7ab64f94febcd9a88f74fa0a3a8624ba
LorakR <stillhet@abv.bg>
2014-07-20 04:17:15 -0700

    .init

---
+++ IntroductionToProgramming/04HelloCSharp/HelloCSharp.cs
@@ -0,0 +1,12 @@
+﻿/*Create, compile and run a "HelloCSharp" console application.*/
+
+using System;
+
+class HelloCSharp
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello C# !");
+    }
+}
+
