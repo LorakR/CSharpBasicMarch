9aff9a6bdbea34b15178cb02fe135d33d223f156
LorakR <stillhet@abv.bg>
2014-07-20 06:52:06 -0700

    .init

---
+++ IntroductionToProgramming/17PlayWithDebbugger/PlayWithDebbugger.cs
@@ -0,0 +1,17 @@
+﻿/*Write a program that prints at the console the numbers from 1 to 1000, each at a separate line. 
+Set a breakpoint in the line, which prints the next number in the Visual Studio code editor. 
+Run the program through the debugger using the [F5] key. 
+When the debugger stops at the breakpoint trace the code execution with [F10] key.*/
+
+using System;
+
+class PlayWithDebugger
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 1000; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
