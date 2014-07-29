5557b5f97d0656cdb7f2765752dee62a885d87be
LorakR <stillhet@abv.bg>
2014-07-29 17:42:49 +0300

    .init

---
+++ 02PrimitiveDataTypesAndVariables/14ComparingFloats/ComparingFloats.cs
@@ -0,0 +1,21 @@
+﻿/*Write a program that safely compares floating-point numbers (double) with precision eps = 0.000001. Note that we cannot directly compare two floating-point numbers a and b by a==b because of the nature of the floating-point arithmetic. Therefore, we assume two numbers are equal if they are more closely to each other than a fixed constant eps.*/
+
+using System;
+
+class ComparingFloats
+{
+    static void Main()
+    {
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        if (Math.Abs(a - b) >= 0.000001)
+        {
+            Console.WriteLine("false");
+        }
+        else
+        {
+           Console.WriteLine("true");
+        }
+    }
+}
+
