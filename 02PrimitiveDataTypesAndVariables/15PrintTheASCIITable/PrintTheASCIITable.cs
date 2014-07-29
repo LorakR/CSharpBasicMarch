eb753b726d7a6fe2f48fc665dfc7bb0e7bf5107c
LorakR <stillhet@abv.bg>
2014-07-29 19:34:19 +0300

    .init

---
+++ 02PrimitiveDataTypesAndVariables/15PrintTheASCIITable/PrintTheASCIITable.cs
@@ -0,0 +1,16 @@
+﻿/*Find online more information about ASCII (American Standard Code for Information Interchange) and write a program to print the entire ASCII table of characters at the console (characters from 0 to 255). Note that some characters have a special purpose and will not be displayed as expected. You may skip them or display them differently. You may need to use for-loops (learn in Internet how).*/
+
+using System;
+
+class PrintTheASCIITable
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.GetEncoding(1252);
+        for (int i = 0; i <= 255; i++)
+        {
+            Console.WriteLine("{0} = {1}",i,(char)i);
+        }
+    }
+}
+
