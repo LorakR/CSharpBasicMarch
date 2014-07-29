0d272c4fa4bd4c81f2dc3deb2bd3c1b6f78d23b5
LorakR <stillhet@abv.bg>
2014-07-29 15:51:22 +0300

    .init

---
+++ 02PrimitiveDataTypesAndVariables/13NullValuesArithmetic/NullValuesArithmetic.cs
@@ -0,0 +1,19 @@
+﻿/*Create a program that assigns null values to an integer and to a double variable. Try to print these variables at the console. Try to add some number or the null literal to these variables and print the result.*/
+
+using System;
+
+class NullValuesArithmetic
+{
+    static void Main()
+    {
+        int? a = null;
+        double? b = null;
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        a = a + 5;
+        b = b + null;
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+    }
+}
+
