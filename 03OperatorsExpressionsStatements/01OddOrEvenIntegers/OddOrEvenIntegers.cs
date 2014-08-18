8a5494e0afab7d6a1a4588e028985d57f6588008
LorakR <stillhet@abv.bg>
2014-08-18 12:44:56 +0300

    .init

---
+++ 03OperatorsExpressionsStatements/01OddOrEvenIntegers/OddOrEvenIntegers.cs
@@ -0,0 +1,15 @@
+﻿/*Write an expression that checks if given integer is odd or even.*/
+
+using System;
+
+class OddOrEvenIntegers
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        //In case the condition is fulfilled the value of the variable isOdd will be true. Otherwise the value will be false.
+        bool isOdd = n % 2 != 0;
+        Console.WriteLine(isOdd);
+    }
+}
+
