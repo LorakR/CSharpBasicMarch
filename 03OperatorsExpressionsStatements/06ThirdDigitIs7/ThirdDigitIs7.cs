3157b3c3e2f12317b4b91e58326ca9abe2a8de9d
LorakR <stillhet@abv.bg>
2014-08-18 13:31:44 +0300

    .init

---
+++ 03OperatorsExpressionsStatements/06ThirdDigitIs7/ThirdDigitIs7.cs
@@ -0,0 +1,16 @@
+﻿/*Write an expression that checks for given integer if its third digit from right-to-left is 7*/
+
+using System;
+
+class ThirdDigitIs7
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        // n/100 takes off the first two digits of the number from right-to-left
+        // %10 checks whether the third digit is seven
+        bool isSeven = (n / 100) % 10 == 7;
+        Console.WriteLine(isSeven);
+    }
+}
+
