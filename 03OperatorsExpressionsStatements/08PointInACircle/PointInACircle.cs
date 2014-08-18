402641df6cc6bde1fc958628a531f2f5637234e3
LorakR <stillhet@abv.bg>
2014-08-18 16:31:28 +0300

    .init

---
+++ 03OperatorsExpressionsStatements/08PointInACircle/PointInACircle.cs
@@ -0,0 +1,15 @@
+﻿/*Write an expression that checks if given point (x,  y) is inside a circle K({0, 0}, 2).*/
+
+using System;
+
+class PointInACircle
+{
+    static void Main()
+    {
+        double x = double.Parse(Console.ReadLine());
+        double y = double.Parse(Console.ReadLine());
+        bool isInCircle = (x * x + y * y <= 2 * 2);
+        Console.WriteLine(isInCircle);
+    }
+}
+
---
+++ 03OperatorsExpressionsStatements/09PrimeNumberCheck/PrimeNumberCheck.cs
@@ -0,0 +1,32 @@
+﻿/*Write an expression that checks if given integer number n (n ≤ 100) is prime (A number is prime if it is bigger than 1 and is divisible without remainder only to itself and 1). */
+
+using System;
+
+class PrimeNumberCheck
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        bool isPrime = true;
+        if (n > 1)
+        {
+            //if the number n divisible by a number different from 1 or itself --> the number is NOT prime
+            //use "break" to stop the loop from unnecessary iterations
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            Console.WriteLine(isPrime);
+        }
+        else
+        {
+            isPrime = false;
+            Console.WriteLine(isPrime);
+        }
+    }
+}
+
