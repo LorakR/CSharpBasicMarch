fb608ca77387a5fa05725da7bf2daf8eea6eb1c1
LorakR <stillhet@abv.bg>
2014-08-19 14:58:00 +0300

    .init

---
+++ 03OperatorsExpressionsStatements/14CheckBitAtGivenPosition/CheckBitAtGivenPosition.cs
@@ -0,0 +1,15 @@
+﻿/*Write a Boolean expression that returns if the bit at position p (counting from 0, starting from the right) in given integer number n has value of 1. */
+using System;
+
+class CheckBitAtGivenPosition
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int position = int.Parse(Console.ReadLine());
+        int mask = 1;
+        bool isValueOne=(((n & (mask << position))>>position)==1);
+        Console.WriteLine(isValueOne);
+    }
+}
+
