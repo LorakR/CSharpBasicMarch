a8ce7496c45a04d155ae69eee76e0d44d7a359de
LorakR <stillhet@abv.bg>
2014-08-19 14:46:32 +0300

    .init

---
+++ 03OperatorsExpressionsStatements/13ExtractBitFromInteger/ExtractBitFromInteger.cs
@@ -0,0 +1,16 @@
+﻿/*Write an expression that extracts from given integer n the value of given bit at index p.*/
+
+using System;
+
+class ExtractBitFromInteger
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int position = int.Parse(Console.ReadLine());
+        int mask = 1;
+        int bitAtWantedPosition = ((n & (1 << position)) >> position);
+        Console.WriteLine(bitAtWantedPosition);
+    }
+}
+
