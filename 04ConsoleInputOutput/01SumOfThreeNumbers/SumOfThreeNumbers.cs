1aaca6d64fcfd8245b6d50559cf36cd0da32843b
LorakR <stillhet@abv.bg>
2014-08-31 19:58:15 +0300

    .init

---
+++ 04ConsoleInputOutput/01SumOfThreeNumbers/SumOfThreeNumbers.cs
@@ -0,0 +1,22 @@
+﻿/*Write a program that reads 3 real numbers from the console and prints their sum.*/
+
+using System;
+//these two using directives allow us to change the culture of the current thread later
+using System.Threading;
+using System.Globalization;
+
+class SumOfThreeNumbers
+{
+    static void Main()
+    {
+        //Number formats are different in different countries e.g the decimal separator can be "." or ","
+        //To ensure that the decimal separator is "." - use the following code
+        //Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        double c = double.Parse(Console.ReadLine());
+        double sum = a + b + c;
+        Console.WriteLine(sum);
+    }
+}
+
