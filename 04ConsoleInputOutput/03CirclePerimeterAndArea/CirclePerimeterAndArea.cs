454d67d6a010f3946b0c688d084946c0d3e89f31
LorakR <stillhet@abv.bg>
2014-09-01 11:55:04 +0300

    .init

--- 04ConsoleInputOutput/03CirclePerimeterAndArea/CirclePerimeterAndArea.cs
+++ 04ConsoleInputOutput/03CirclePerimeterAndArea/CirclePerimeterAndArea.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*Write a program that reads the radius r of a circle and prints its perimeter and area formatted with 2 digits after the decimal point. */
 
-namespace _03CirclePerimeterAndArea
+using System;
+
+class CirclePerimeterAndArea
 {
-    class CirclePerimeterAndArea
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-        }
+        double radius = double.Parse(Console.ReadLine());
+        double area = Math.PI * Math.Pow(radius, 2);
+        double perimeter = 2 * Math.PI * radius;
+        //{index[,alignment][:formatString]} ---> formatString specifies the format of the corresponding arguments
+        Console.WriteLine("The area of the circle is {0:0.00}",area);
+        Console.WriteLine("The perimeter of the cirle is {0:0.00}",perimeter);
     }
 }
+
