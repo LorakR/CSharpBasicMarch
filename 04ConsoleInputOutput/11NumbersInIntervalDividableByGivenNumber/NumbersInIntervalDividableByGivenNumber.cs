322e27391f963a27635083b2b660f3405fab9b9d
LorakR <stillhet@abv.bg>
2014-09-01 20:30:16 +0300

    .init

---
+++ 04ConsoleInputOutput/11NumbersInIntervalDividableByGivenNumber/NumbersInIntervalDividableByGivenNumber.cs
@@ -0,0 +1,21 @@
+﻿/*Write a program that reads two positive integer numbers and prints how many numbers p exist between them such that the reminder of the division by 5 is 0.*/
+using System;
+
+class NumbersInIntervalDividableByGivenNumber
+{
+    static void Main()
+    {
+        int start = int.Parse(Console.ReadLine());
+        int end = int.Parse(Console.ReadLine());
+        int count = 0;
+        for (int i = start; i <= end; i++)
+        {
+            if (i % 5 == 0)
+            {
+                count++;
+            }
+        }
+        Console.WriteLine("Between {0} and {1} there are {2} numbers dividable by 5.",start,end,count);
+    }
+}
+
