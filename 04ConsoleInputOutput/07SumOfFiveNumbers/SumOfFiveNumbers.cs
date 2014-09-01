9e75edd8a21917704356793fef7e555db220ada2
LorakR <stillhet@abv.bg>
2014-09-01 19:42:58 +0300

    .init

--- 04ConsoleInputOutput/07SumOfFiveNumbers/SumOfFiveNumbers.cs
+++ 04ConsoleInputOutput/07SumOfFiveNumbers/SumOfFiveNumbers.cs
@@ -6,6 +6,8 @@ class SumOfFiveNumbers
 {
     static void Main()
     {
+        //use array of type string
+        //.Split(' ') saves the value written before the first space of the entered string at the cell with index[0], the value before the second space at the cell with index[1] and so on..
         string[] fiveNumbers = Console.ReadLine().Split(' ');
         double a = double.Parse(fiveNumbers[0]);
         double b = double.Parse(fiveNumbers[1]);
---
+++ 04ConsoleInputOutput/10FibonacciNumbers/FibonacciNumbers.cs
@@ -0,0 +1,34 @@
+﻿/*Write a program that reads a number n and prints on the console the first n members of the Fibonacci sequence (at a single line, separated by spaces) : 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ….*/
+
+using System;
+
+class FibonacciNumbers
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int a = 0;
+        int b = 1;
+        int c;
+        if (n == 1)
+        {
+            Console.WriteLine(a);
+        }
+        else if (n == 2)
+        {
+            Console.WriteLine("{0} {1}",a,b);
+        }
+        else
+        {
+            Console.Write("{0} {1} ", a, b);
+            for (int i = 1; i <= n - 2; i++)
+            {
+                c = a;
+                a = b;
+                b = c + a;
+                Console.Write("{0} ",b);
+            }
+        }
+    }
+}
+
