c0bd66c43a36aa4debc5548cc20d97b5c8c244a5
LorakR <stillhet@abv.bg>
2014-09-01 17:03:17 +0300

    .init

--- 04ConsoleInputOutput/06QuadraticEquation/QuadraticEquation.cs
+++ 04ConsoleInputOutput/06QuadraticEquation/QuadraticEquation.cs
@@ -12,18 +12,24 @@ class QuadraticEquation
         double b = double.Parse(Console.ReadLine());
         Console.Write("c = ");
         double c = double.Parse(Console.ReadLine());
+        double root1 = 0;
+        double root2 = 0;
         if (a == 0)
         {
-            Console.WriteLine("This equation is not quadratic and has one real root: {0}", -c / b);
+            root1 = -c / b;
+            Console.WriteLine("This equation is not quadratic and has one real root: {0}", root1);
         }
         double discriminant = Math.Sqrt(Math.Pow(b, 2) - 4 * a * c);
         if (discriminant == 0)
         {
-            Console.WriteLine("The equation has one real root: x1={0}", -b /2 * a);
+            root1 = -b /( 2 * a);
+            Console.WriteLine("The equation has one real root: x1={0}",root1);
         }
         else if (discriminant > 0)
         {
-            Console.WriteLine("The equation has two real roots: x1={0} and x2={1}", (-b + discriminant) / 2 * a, (-b - discriminant) / 2 * a);
+            root1 = (-b + discriminant) / (2 * a);
+            root2 = (-b - discriminant) / (2 * a);
+            Console.WriteLine("The equation has two real roots: x1={0} and x2={1}", root1, root2);
         }
         else
         {
