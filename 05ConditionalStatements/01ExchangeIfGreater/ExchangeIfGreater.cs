a3ddf92d7d0bb5c12bd9ce2f6aa0778cf656e8ee
LorakR <stillhet@abv.bg>
2014-09-09 19:05:50 +0300

    .init

---
+++ 05ConditionalStatements/01ExchangeIfGreater/ExchangeIfGreater.cs
@@ -0,0 +1,21 @@
+﻿/*Write an if-statement that takes two variables a and b and exchanges their values if the first one is greater than the second one. As a result prdouble the values a and b, separated by a space.*/
+
+using System;
+
+class ExchangeIfGreater
+{
+    static void Main()
+    {
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        double c;
+        if (a > b)
+        {
+            c = b;
+            b = a;
+            a = c;
+        }
+        Console.WriteLine("{0} {1}", a, b);
+    }
+}
+
