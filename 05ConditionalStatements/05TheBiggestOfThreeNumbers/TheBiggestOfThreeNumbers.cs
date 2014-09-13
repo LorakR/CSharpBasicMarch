8cc97a6dc00c1f0ab18b924703375f049f31030d
LorakR <stillhet@abv.bg>
2014-09-13 17:15:01 +0300

    .init

---
+++ 05ConditionalStatements/05TheBiggestOfThreeNumbers/TheBiggestOfThreeNumbers.cs
@@ -0,0 +1,22 @@
+﻿/*Write a program that finds the biggest of three numbers. */
+
+using System;
+
+class TheBiggestOfThreeNumbers
+{
+    static void Main()
+    {
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        double c = double.Parse(Console.ReadLine());
+        if (c > Math.Max(a, b))
+        {
+            Console.WriteLine(c);
+        }
+        else
+        {
+            Console.WriteLine(Math.Max(a,b));
+        }
+    }
+}
+
